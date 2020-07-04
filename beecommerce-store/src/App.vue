cc88483fc3a3852b57b93425d2e50c57cb26793a
marin_cordivin <mcordivin@users.noreply.github.com>
2020-07-04 14:09:47 -0700

    routing done

---
+++ beecommerce-store/src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <nav>
+      <div class="container">
+        <ul class="nav__left">
+          <li><router-link to="/">Home</router-link></li>
+          <li><router-link to="/admin">Admin</router-link></li>
+          <li><router-link to="/cart">Cart</router-link></li>
+        </ul>
+      </div>
+    </nav>
+
+    <router-view></router-view>
+  </div>
+</template>
